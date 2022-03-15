c29a57947131ba4ef7478bcf12f5d1670188e28c
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-03-15 15:51:18 +1000

    Add SongsService and Intent from MainActivity.

--- app/src/main/java/com/example/cp3406utilityapp/MainActivity.java
+++ app/src/main/java/com/example/cp3406utilityapp/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.cp3406utilityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Button getSongsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // This is a comment.
+
+        getSongsBtn = (Button) findViewById(R.id.btn_getSongs);
+        getSongsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
