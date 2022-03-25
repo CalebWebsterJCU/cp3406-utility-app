925a4814449f65f09a73964efe5791d6210fb73d
CalebWebsterJCU <caleb.webster@my.jcu.edu.au>
2022-03-25 18:20:18 +1000

    Remove commented-out code.

--- app/src/test/java/com/example/cp3406utilityapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/cp3406utilityapp/ExampleUnitTest.java
@@ -18,21 +18,4 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
-//    @Test
-//    public void serviceCanConnect() {
-//        TopSongsService service = new TopSongsService("87f7150f2a784eb0bc252cc29436f4af", "ba29f02133bb422487493547c3f2fa04");
-//        assertTrue(service.connectClientCredentials());
-//    }
-//
-//    @Test
-//    public void serviceCanGetTopSongs() {
-//        TopSongsService service = new TopSongsService("87f7150f2a784eb0bc252cc29436f4af", "ba29f02133bb422487493547c3f2fa04");
-//        service.connectClientCredentials();
-//        ArrayList<Track> tracks = service.getTopSongs();
-//        assertNotNull(tracks);
-//        for (Track track : tracks) {
-//            System.out.println(track.getName());
-//        }
-//    }
 }
